2d2029f2c06e78d983bd4b7d78f168aad4090367
Bojan Kljakic <bojan.kljakic@orchestranetworks.com>
2014-02-18 17:54:35 +0100

    Sample change

--- EBX-Project/src/com/orchestranetworks/ps/module/RegisterServlet.java
+++ EBX-Project/src/com/orchestranetworks/ps/module/RegisterServlet.java
@@ -6,14 +6,13 @@ package com.orchestranetworks.ps.module;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import com.onwbp.base.repository.*;
-
 /**
  */
 public class RegisterServlet extends HttpServlet
 {
 	public void init(ServletConfig config) throws ServletException
 	{
+		// Small change to test forks
 		super.init(config);
 		ModulesRegister.registerWebApp(this, config);
 	}
